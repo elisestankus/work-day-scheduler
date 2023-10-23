7e14ef9659672de404f34ae976deadbfb1e78331
estan <elise.stankus@gmail.com>
2023-10-23 14:18:47 -0700

    added save hour functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,6 +53,69 @@ $(function () {
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
+
+  function saveHour9() {
+    var hour9 = hour9Input.val();
+    localStorage.setItem("hour9", JSON.stringify(hour9));
+    storeHour9();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour10() {
+    var hour10 = hour10Input.val();
+    localStorage.setItem("hour10", JSON.stringify(hour10));
+    storeHour10();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour11() {
+    var hour11 = hour11Input.val();
+    localStorage.setItem("hour11", JSON.stringify(hour11));
+    storeHour11();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour12() {
+    var hour12 = hour12Input.val();
+    localStorage.setItem("hour12", JSON.stringify(hour12));
+    storeHour12();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour13() {
+    var hour13 = hour13Input.val();
+    localStorage.setItem("hour13", JSON.stringify(hour13));
+    storeHour13();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour14() {
+    var hour14 = hour14Input.val();
+    localStorage.setItem("hour14", JSON.stringify(hour14));
+    storeHour14();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour15() {
+    var hour15 = hour15Input.val();
+    localStorage.setItem("hour15", JSON.stringify(hour15));
+    storeHour15();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour16() {
+    var hour16 = hour16Input.val();
+    localStorage.setItem("hour16", JSON.stringify(hour16));
+    storeHour16();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
+
+  function saveHour17() {
+    var hour17 = hour17Input.val();
+    localStorage.setItem("hour17", JSON.stringify(hour17));
+    storeHour17();
+    confirmationMessage.text('Appointment saved to local storage');
+  }
   //
   // TODO: Add code to display the current date in the header of the page.
   var currentDate = dayjs().format('dddd[,] MMMM D[,] YYYY');
