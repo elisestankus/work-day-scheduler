ad6e879aaac7bd5fdb642fb9b97704eb1809cd22
estan <elise.stankus@gmail.com>
2023-10-23 13:06:24 -0700

    declared currentDate var

--- assets/js/script.js
+++ assets/js/script.js
@@ -33,4 +33,5 @@ $(function () {
   // attribute of each time-block be used to do this?
   //
   // TODO: Add code to display the current date in the header of the page.
+  var currentDate = dayjs().format('dddd[,] MMMM D[,] YYYY');
 });
