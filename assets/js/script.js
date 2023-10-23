c3bafdb611ca43b240c03091da46f4fea4cea41d
estan <elise.stankus@gmail.com>
2023-10-23 14:12:33 -0700

    declared confirm message var

--- assets/js/script.js
+++ assets/js/script.js
@@ -22,6 +22,8 @@ $(function () {
   var hour15Input = $('textarea[name = "hour15Input"]');
   var hour16Input = $('textarea[name = "hour16Input"]');
   var hour17Input = $('textarea[name = "hour17Input"]');
+
+  var confirmationMessage = $('#confirm-message')
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
